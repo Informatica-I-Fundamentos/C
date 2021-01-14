db5e434e3c0f13d183114407466e16ee28ac1f5d
Dannas Cornell <dannas.cornell@gmail.com>
2021-01-13 18:46:46 -0600

    Update hola.c

--- hola.c
+++ hola.c
@@ -1,10 +1,15 @@
 //Librerías
 #include <stdio.h> //Entradas y Salidas
+/*
+  Autor: Daniel Rosales
+  Fecha: 13012021
+  Descripción: Ejemplo Hola Mundo
+*/
 
 //Función principal
 
-int main(void){ //
-   printf("\n\tHola Mundo\n");
+int main(void){ 
+   printf("\n\tHola Mundo\n");  //Etiqueta
    return 0;
 }
 
